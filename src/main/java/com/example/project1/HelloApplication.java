7179ce62ec52b91a12d5d9be9c28c97bcd660bbf
dhayling19 <82467478+dhayling19@users.noreply.github.com>
2022-11-09 20:57:48 -0300

    cleanup

--- src/main/java/com/example/project1/HelloApplication.java
+++ src/main/java/com/example/project1/HelloApplication.java
@@ -1,6 +1,5 @@
 package com.example.project1;
 
-
 public class HelloApplication {
 
     public static void main(String[] args) {
@@ -17,14 +16,13 @@ public class HelloApplication {
 
     }
 
-
-
     public static int suma (int a, int b, int c){
         return a+b+c;
 
     }
 
 }
+
 class Coche {
     public int nPuertas = 0;
 
