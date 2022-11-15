dedd514b82902f0bfad52e9bbb8e2225c08d7e49
dhayling19 <82467478+dhayling19@users.noreply.github.com>
2022-11-15 17:14:36 -0300

    Todos los ejercicios separados en funciones.

--- src/main/java/com/example/project1/HelloApplication.java
+++ src/main/java/com/example/project1/HelloApplication.java
@@ -2,27 +2,78 @@ package com.example.project1;
 
 public class HelloApplication {
 
+    // Ejercicio if
+    public static void sieltu(int numerolf) {
+
+        if (numerolf > 0) {
+            System.out.println("El numero es positivo");
+        } else if (numerolf < 0) {
+            System.out.println("El numero es negativo");
+        } else {
+            System.out.println("El numero es 0");
+        }
+    }
+
+    // Ejercicio While
+    public static void wie(int numeroWhile) {
+        while (numeroWhile < 3) {
+            System.out.println(numeroWhile);
+            numeroWhile++;
+        }
+    }
+
+    public static void dowie(int numeroWhilel) {
+        do {
+            System.out.println(numeroWhilel);
+            numeroWhilel++;
+        } while (numeroWhilel < 0);
+
+    }
+
+    //Ejercicio Switch
+    public static void casitos(String estacion) {
+        switch (estacion) {
+            case "summer":
+                System.out.println("Usted esta en verano");
+                break;
+            case "winter":
+                System.out.println("Usted esta en invierno");
+                break;
+            case "spring":
+                System.out.println("Usted esta en  primavera");
+                break;
+            case "fall":
+                System.out.println("Uste esta en otoño");
+                break;
+            default:
+                System.out.println("It's not a station");
+        }
+    }
+
+    // Ejercicio for
+    public static void paraMi(int numeroFor) {
+        for (numeroFor = 0; numeroFor <= 3; numeroFor++) {
+            System.out.println(numeroFor);
+        }
+    }
+
     public static void main(String[] args) {
-        int result;
-        result = suma(10, 30, 25);
-        System.out.println(result);
+        // Llamando a la funcion del ejercicio while
+        wie(0);
 
-        Coche miCoche = new Coche();
-        miCoche.incrementPuertas();
-        miCoche.incrementPuertas();
-        miCoche.incrementPuertas();
-        System.out.println(miCoche.nPuertas);
-    }
+        //Llamando a la funcion del ejercicio if
+        sieltu(20);
 
-    public static int suma (int a, int b, int c){
-        return a+b+c;
-    }
-}
+        // llamando a la funcion del ejercicio doWhile
+        dowie(1);
 
-class Coche {
-    public int nPuertas = 4;
+        // Llamando a la funcion del ejercicio Switch
 
-    public void incrementPuertas() {
-        this.nPuertas++;
+        casitos("fall");
+
+        // llamando a la funcion del ejercicio for
+        paraMi(0);
+
+
     }
 }
