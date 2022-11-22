01466150e2ec33a8360db5a693cde2ad95c75319
dhayling19 <82467478+dhayling19@users.noreply.github.com>
2022-11-22 13:42:31 -0300

    Creando clase persona y sus datos.

--- src/main/java/com/example/project1/HelloApplication.java
+++ src/main/java/com/example/project1/HelloApplication.java
@@ -2,78 +2,34 @@ package com.example.project1;
 
 public class HelloApplication {
 
-    // Ejercicio if
-    public static void sieltu(int numerolf) {
-
-        if (numerolf > 0) {
-            System.out.println("El numero es positivo");
-        } else if (numerolf < 0) {
-            System.out.println("El numero es negativo");
-        } else {
-            System.out.println("El numero es 0");
-        }
-    }
-
-    // Ejercicio While
-    public static void wie(int numeroWhile) {
-        while (numeroWhile < 3) {
-            System.out.println(numeroWhile);
-            numeroWhile++;
-        }
-    }
-
-    public static void dowie(int numeroWhilel) {
-        do {
-            System.out.println(numeroWhilel);
-            numeroWhilel++;
-        } while (numeroWhilel < 0);
-
-    }
-
-    //Ejercicio Switch
-    public static void casitos(String estacion) {
-        switch (estacion) {
-            case "summer":
-                System.out.println("Usted esta en verano");
-                break;
-            case "winter":
-                System.out.println("Usted esta en invierno");
-                break;
-            case "spring":
-                System.out.println("Usted esta en  primavera");
-                break;
-            case "fall":
-                System.out.println("Uste esta en otoño");
-                break;
-            default:
-                System.out.println("It's not a station");
-        }
-    }
-
-    // Ejercicio for
-    public static void paraMi(int numeroFor) {
-        for (numeroFor = 0; numeroFor <= 3; numeroFor++) {
-            System.out.println(numeroFor);
-        }
-    }
-
     public static void main(String[] args) {
-        // Llamando a la funcion del ejercicio while
-        wie(0);
-
-        //Llamando a la funcion del ejercicio if
-        sieltu(20);
-
-        // llamando a la funcion del ejercicio doWhile
-        dowie(1);
-
-        // Llamando a la funcion del ejercicio Switch
-
-        casitos("fall");
-
-        // llamando a la funcion del ejercicio for
-        paraMi(0);
-
-
+        Persona persona = new Persona();
+        persona.setEdad(26);
+        persona.setNombre("Dhayling");
+        persona.setTelefono(25896433);
+        System.out.println("Hola " + persona.getNombre()+ " Tienes " + persona.getEdad() + " años. Y tu numero de telefono es " + persona.getTelefono());
+    }
+}
+class Persona {
+    private  int edad;
+    private String nombre;
+    private int telefono;
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void setTelefono(int telefono){
+        this.telefono = telefono;
+    }
+    public int getTelefono(){
+        return telefono;
     }
 }
