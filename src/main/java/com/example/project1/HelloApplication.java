011d570a726f14ec4684a851a24e00f54de7be31
dhayling19 <82467478+dhayling19@users.noreply.github.com>
2022-12-06 18:13:02 -0300

    Creando la clase y datos

--- src/main/java/com/example/project1/HelloApplication.java
+++ src/main/java/com/example/project1/HelloApplication.java
@@ -3,26 +3,31 @@ package com.example.project1;
 public class HelloApplication {
 
     public static void main(String[] args) {
-        int result;
-        result = suma(10, 30, 25);
-        System.out.println(result);
 
-        Coche miCoche = new Coche();
-        miCoche.incrementPuertas();
-        miCoche.incrementPuertas();
-        miCoche.incrementPuertas();
-        System.out.println(miCoche.nPuertas);
     }
 
-    public static int suma (int a, int b, int c){
-        return a+b+c;
-    }
+
 }
 
-class Coche {
-    public int nPuertas = 4;
 
-    public void incrementPuertas() {
-        this.nPuertas++;
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/example/project1/MiPrimeraClse.java
@@ -0,0 +1,40 @@
+package com.example.project1;
+
+public class MiPrimeraClse {
+    public static void main(String[] args) {
+
+        byte number = 1;
+        System.out.println("Un byte es " + number);
+
+        short number2 = 2;
+        System.out.println("Un short es " + number2);
+
+        int number3 = 3;
+        System.out.println("Un int es " + number3);
+
+        long number4 = 4;
+        System.out.println("Un long es " + number4);
+
+        float decimal = 5.9f;
+        System.out.println("Un float es " + decimal);
+
+
+        double decimal2 = 6.33;
+        System.out.println("Un float es " + decimal2);
+
+        char caracter1 = 'd';
+        System.out.println("Un caracter es "+ caracter1);
+
+
+        String color = "Rosado";
+        System.out.println("Estas viendo un String que contiene un color  "+ color);
+
+        boolean verdad = true;
+        System.out.println("Si dices la verdad es " + verdad);
+
+        boolean mentira = false;
+        System.out.println("Si dices mentiras es " + mentira);
+
+
+    }
+}
