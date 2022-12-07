7875e009845c7a827218124fb6f8b2575a3c3203
dhayling19 <82467478+dhayling19@users.noreply.github.com>
2022-12-07 15:58:20 -0300

    Calculando el Iva de un precio.

--- src/main/java/com/example/project1/HelloApplication.java
+++ src/main/java/com/example/project1/HelloApplication.java
@@ -3,10 +3,14 @@ package com.example.project1;
 public class HelloApplication {
 
     public static void main(String[] args) {
-
+        System.out.println(calcularIva(158));
+        System.out.println(calcularIva(75));
     }
 
-
+    static double calcularIva(double precio){
+        double iva = precio * 5.8 / 100;
+        return precio + iva;
+    }
 }
 
 
